a666acec7cd214ce5bb360f6667f96a610d0dd2b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-09-19 03:22:13 -0400

    Allow moving nodes

--- src/InputManager.js
+++ src/InputManager.js
@@ -6,22 +6,56 @@ class InputManager{
         console.log("space: "+this.space);
         this.mouseOverNode = undefined;
         document.onmousemove = this.onMouseMove;
+        this.mouseClick = false;
+        this.clickOffset = {
+            x:0,
+            y:0
+        };
+        document.onmousedown = this.onMouseDown;
+        document.onmouseup = this.onMouseUp;
     }
 
     onMouseMove(e){
-        let prevMouseOverNode = inputManager.mouseOverNode;
-        inputManager.mouseOverNode = undefined;
         let x = inputManager.space.convertFromX(e.x);
         let y = inputManager.space.convertFromY(e.y);
-        for(let nodeIndex in inputManager.space.nodeList){
-            if (!nodeIndex){continue;}
-            let node = inputManager.space.nodeList[nodeIndex];
-            if (node.inBounds(x, y)){
-                inputManager.mouseOverNode = node;
-                break;
+        if (inputManager.mouseClick){
+            inputManager.mouseOverNode.setPosition(
+                x + inputManager.clickOffset.x,
+                y + inputManager.clickOffset.y
+            );
+            display.draw();
+        }
+        else{
+            let prevMouseOverNode = inputManager.mouseOverNode;
+            inputManager.mouseOverNode = undefined;
+            for(let nodeIndex in inputManager.space.nodeList){
+                if (!nodeIndex){continue;}
+                let node = inputManager.space.nodeList[nodeIndex];
+                if (node.inBounds(x, y)){
+                    inputManager.mouseOverNode = node;
+                    break;
+                }
+            }
+            if (inputManager.mouseOverNode != prevMouseOverNode){
+                display.draw();
             }
         }
-        if (inputManager.mouseOverNode != prevMouseOverNode){
+    }
+
+    onMouseDown(e){
+        let x = inputManager.space.convertFromX(e.x);
+        let y = inputManager.space.convertFromY(e.y);
+        if (inputManager.mouseOverNode){
+            inputManager.clickOffset.x = inputManager.mouseOverNode.position.x - x;
+            inputManager.clickOffset.y = inputManager.mouseOverNode.position.y - y;
+            inputManager.mouseClick = true;
+            display.draw();
+        }
+    }
+
+    onMouseUp(e){
+        inputManager.mouseClick = false;
+        if (inputManager.mouseOverNode){
             display.draw();
         }
     }
