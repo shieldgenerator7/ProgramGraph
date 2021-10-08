f2e3e8c4a9a38eb3076c32affc96e05f584d714a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-10-07 21:45:54 -0400

    Add panel factory template

---
+++ src/_TEMPLATE_TypePanelFactory.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function [Type]PanelFactory(panel){
+    //
+    // State
+    //
+
+    //
+    // Display
+    //
+
+    //
+    // Input
+    //
+
+    //
+    // Control
+    //
+
+    //
+    // Verify
+    //
+
+    //
+    // Autolayout
+    //
+
+    //
+    // File
+    //
+}
